285ae7252c20fe8b038bfe78539d939ca5a9e743
_dream <will.ferens@gmail.com>
2018-09-10 15:45:00 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { Prisma } = require('prisma-binding')
+
+
+
+const resolvers = {
+    Query: {
+        info: () => `This is the API`,
+        feed: (root, args, context, info) => {
+            return context.db.query.links({}, info)
+        }
+    },
+    Mutation: {
+        post: (root, args, context, info) => {
+            return context.db.mutation.createLink({
+                data: {
+                    url: args.url,
+                    description: args.description
+                }
+            }, info)
+        }
+    },
+}
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    context: req => ({
+        ...req,
+        db: new Prisma({
+            typeDefs: 'src/generated/prisma.graphql',
+            endpoint: 'https://us1.prisma.sh/willferens/database/dev',
+            secret: 'poopypoopybuttholes69',
+            debug: true
+        })
+    })
+})
+
+server.start(() => console.log('Server is running on http://localhost:4000'))
