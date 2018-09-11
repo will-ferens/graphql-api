6611d3dcf5aa1e440c75d37b1c233d562bfa111c
_dream <will.ferens@gmail.com>
2018-09-11 13:15:07 -0600

    added filters and pagination

--- src/index.js
+++ src/index.js
@@ -1,25 +1,20 @@
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const AuthPayload = require('./resolvers/AuthPayload.js')
+const Subscription = require('./resolvers/Subscriptions')
+const Feed = require('./resolvers/Feed')
+
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 
 
 
 const resolvers = {
-    Query: {
-        info: () => `This is the API`,
-        feed: (root, args, context, info) => {
-            return context.db.query.links({}, info)
-        }
-    },
-    Mutation: {
-        post: (root, args, context, info) => {
-            return context.db.mutation.createLink({
-                data: {
-                    url: args.url,
-                    description: args.description
-                }
-            }, info)
-        }
-    },
+    Query,
+    Mutation,
+    AuthPayload,
+    Subscription,
+    Feed
 }
 
 const server = new GraphQLServer({
